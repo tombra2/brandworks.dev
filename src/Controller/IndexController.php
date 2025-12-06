de818d5b8eccea50d792a4a19b6fc985dee3e608
tom_brandi <brandner.thomas@me.com>
2025-12-06 13:00:31 +0100

    Update logger test

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(LoggerInterface $logger): Response
     {
+
+        $logger->error('Test-Log aus Controller', [
+            'time' => new \DateTimeImmutable()->format(DATE_ATOM),
+        ]);
+
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
